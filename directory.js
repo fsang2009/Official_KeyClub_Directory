b4e8fb0fc727268639c2efe7014cb1b629ed4985
Filemon Sang <filemonsang@gmail.com>
2026-09-08 18:26:27 -0400

    firebase implemented, should be connecting

--- directory.js
+++ directory.js
@@ -1,16 +1,113 @@
-const addStudentButton = document.querySelector('#add-student-button');
-const studentInfo = document.querySelector('')
+import { database } from './firebaseconfig';
+import { getDoc, doc, collection, addDoc} from 'firebase/firestore';
 
-const addStudent =()=>{
-
-}
-
+const addStudentButton = document.getElementById('add-student-button');
 const studentList = document.querySelector('#student-info-container');
-const renderStudentList = () =>{
+document.addEventListener('DOMContentLoaded', renderStudentList());
 
+const renderStudentList = () =>{
+    let html ='';
+    
+    const userCollection = collection(database, "users");
+
+    userCollection.forEach((doc)=>{
+        html += `
+        <div class="student-info-bar">
+
+    <div class="student-main-info">
+        <div class="student-name">
+            ${doc.firstName} ${doc.lastName}
+        </div>
+
+        <div class="student-basic-info">
+            <span>ID: ${doc.studentID}</span>
+            <span>Grade: ${doc.studentGrade}</span>
+        </div>
+    </div>
+
+
+    <div class="student-stats">
+
+        <div class="student-stat">
+            Hours: ${doc.hours}
+        </div>
+
+        <div class="student-stat">
+            Points:${doc.points}
+        </div>
+
+    </div>
+
+</div>
+        `;
+    })
+
+    studentList.innerHTML = html;
 } 
 
-addStudentButton.addEventListener('click',()=>{
-    addStudent();
+const addStudentForm =  document.getElementById('add-student-form');
+
+addStudentForm.addEventListener('submit', async(event)=>{
+    event.preventDefault();
+    const studentID = document.getElementById('student-id');
+    const studentFirstName = document.getElementById('first-name');
+    const studentLastName = document.getElementById('last-name');
+    const studentGrade = document.getElementById('grade');
+
+    try{ const newStudent = await addDoc(
+        collection(database, "users"), {
+            studentID: studentID,
+            fistName: studentFirstName,
+            lastName: studentLastName,
+            grade: studentGrade,
+
+            hours: 0,
+            points: 0,
+            feedPaid: false,
+        }
+    )   
+
+    addStudentForm.reset();
+    closeModal();
     renderStudentList();
+    }catch(error){
+        console.log(error.message);
+    }
+})
+
+const addStudentModal = document.getElementById('add-student-modal');
+const closeStudentModal = document.getElementById('modal-close-btn');
+const submitStudentAddButton = document.getElementById('modal-submit-btn');
+const cancelStudentAddButton = document.getElementById('modal-cancel-btn');
+
+const studentID = document.getElementById('student-id');
+const studentFirstName = document.getElementById('first-name');
+const studentLastName = document.getElementById('last-name');
+const studentGrade = document.getElementById('grade');
+
+
+const closeModal = ()=>{
+    addStudentModal.classList.remove('active');
+    studentID.value = '';
+    studentFirstName.value ='';
+    studentLastName.value = '';
+    studentGrade.value = '';
+}
+
+cancelStudentAddButton.addEventListener('click',()=>{
+    closeModal();
+})
+
+addStudentButton.addEventListener('click',()=>{
+    addStudentModal.classList.add('active');
+})
+
+closeStudentModal.addEventListener('click',()=>{
+    closeModal();
+})
+
+submitStudentAddButton.addEventListener('click',()=>{
+    addStudent();
+    closeModal();
+    
 })
---
+++ firebaseconfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCu1oP7uGh7Oq81VGA6uV2_1MLxdX-dmcA",
+  authDomain: "keyclubdirectory.firebaseapp.com",
+  projectId: "keyclubdirectory",
+  storageBucket: "keyclubdirectory.firebasestorage.app",
+  messagingSenderId: "684928184964",
+  appId: "1:684928184964:web:d902a117e096e7985c48d1",
+  measurementId: "G-GXPZKMKN8N"
+};
+
+// Initialize Firebase
+const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+const database = getFirestore (app);
+export { database }
