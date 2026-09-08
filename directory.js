e6c9c87e44ce57d874147bf378b159b112f0e799
Filemon Sang <filemonsang@gmail.com>
2026-09-08 04:12:02 -0400

    js update

---
+++ directory.js
@@ -0,0 +1,16 @@
+const addStudentButton = document.querySelector('#add-student-button');
+const studentInfo = document.querySelector('')
+
+const addStudent =()=>{
+
+}
+
+const studentList = document.querySelector('#student-info-container');
+const renderStudentList = () =>{
+
+} 
+
+addStudentButton.addEventListener('click',()=>{
+    addStudent();
+    renderStudentList();
+})
